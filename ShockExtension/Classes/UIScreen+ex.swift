e3e09a70355c07891786b84e823aa159103019d7
ShockUtility <shock@docs.kr>
2017-05-08 15:34:35 +0900

    UIScreen 추가

---
+++ ShockExtension/Classes/UIScreen+ex.swift
@@ -0,0 +1,26 @@
+//
+//  UIScreen+ex.swift
+//  Pods
+//
+//  Created by shock on 2017. 5. 8..
+//
+//
+
+import UIKit
+
+extension UIScreen {
+    
+    static func realSize(orientation: UIInterfaceOrientation) -> CGSize {
+        let size = UIScreen.main.bounds.size
+        let realSize = CGSize(width: min(size.width, size.height), height: max(size.width, size.height))
+        
+        return orientation.isPortrait ?  realSize : CGSize(width: realSize.height, height: realSize.width)
+    }
+    
+    static func realSize(isLandscape: Bool) -> CGSize {
+        let size = UIScreen.main.bounds.size
+        let realSize = CGSize(width: min(size.width, size.height), height: max(size.width, size.height))
+        
+        return isLandscape ? CGSize(width: realSize.height, height: realSize.width) : realSize
+    }
+}
